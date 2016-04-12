246cd1b8657d9bc63bf55e4139eb0271c2ea8670
Warteruzannan <warteruzannan@gmail.com>
2016-04-12 19:23:30 -0300

    Project Created

---
+++ fiscalizaBr/fiscalizaBr/AgeementViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AgeementViewController.swift
+//  fiscalizaBr
+//
+//  Created by Warteruzannan on 4/12/16.
+//  Copyright © 2016 FiscalizaBr. All rights reserved.
+//
+
+import UIKit
+
+class AgeementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
---
+++ fiscalizaBr/fiscalizaBr/FavoriteViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteViewController.swift
+//  fiscalizaBr
+//
+//  Created by Warteruzannan on 4/12/16.
+//  Copyright © 2016 FiscalizaBr. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
